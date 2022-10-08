c88312f5b85dfae4eb45ed8b1e2c8c04ed01f2ed
Marcelo Augusto <77033790+Marcelo-A-O-S@users.noreply.github.com>
2022-10-08 09:05:27 -0300

    Commit do exercício 1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program 
+{
+    public static void Main() 
+    {
+        Console.WriteLine("Olá, esse é o programa relacionado ao exercicio 1, aperte qualquer tecla para continuar:");
+        Console.ReadKey(true);
+        Ola();
+    }
+
+    private static void Ola()
+    {
+        Console.WriteLine("Olá mundo!");
+    }
+}
