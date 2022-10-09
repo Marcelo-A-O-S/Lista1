f84e62f0d481f4152370f03f30cd0757587bcded
Marcelo Augusto <77033790+Marcelo-A-O-S@users.noreply.github.com>
2022-10-09 17:28:06 -0300

    Commit
    Commit dos exercicios 7 e 8

---
+++ Exercicio8WPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Exercicio8WPF.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercicio8WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dados_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextDistancia_TextChanged(object sender, TextChangedEventArgs e)
+        {
+         
+            
+            
+        }
+
+        private void Acao_Click(object sender, RoutedEventArgs e)
+        {
+            Metro metro = new Metro();
+            metro.metro = float.Parse(TextDistancia.Text);
+            var obj = metro.distancia(metro);
+            Dados.Items.Add(obj);
+            
+        }
+    }
+}
---
+++ Exercicio8WPF/Modelo/Metro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio8WPF.Modelo
+{
+    public class Metro
+    {
+        public float metro { get; set; }
+        public float? km { get; set; }
+        public float? hm { get; set; }
+        public float? dam { get; set; }
+        public float? dm { get; set; }
+        public float? cm { get; set; }
+        public float? mm { get;set; }
+        
+        public Metro distancia(Metro distancia) 
+        {
+            distancia.dm = distancia.metro * 10;
+            distancia.cm = (distancia.metro * 10) * 10;
+            distancia.mm = distancia.metro * 10* 10* 10;
+            distancia.dam = distancia.metro / 10;
+            distancia.hm = (distancia.metro/10) / 10;
+            distancia.km = distancia.metro / 10/10/10;
+            return distancia;
+        }
+    }
+}
